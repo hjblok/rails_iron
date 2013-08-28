e6d11a9aae5735d1b96563b5101689f60ce7d583
H.J. Blok <hj.blok@sudoit.nl>
2013-08-28 16:24:33 +0200

    to make iron_task_id global available

--- lib/rails_iron.rb
+++ lib/rails_iron.rb
@@ -1,4 +1,8 @@
+# make iron_task_id global available
+if respond_to?("iron_task_id")
+  $iron_task_id = iron_task_id
+end
+
 require_relative "rails_iron/error"
 require_relative "rails_iron/loader"
 require_relative "rails_iron/worker"
-
